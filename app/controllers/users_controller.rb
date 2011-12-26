a86c264f7a094732ebd8a4fc29b911312a1d4537
Jeff Saracco <jeffsaracco@gmail.com>
2011-12-25 19:46:04 -0500

    signup without tests yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
   
   def new
+    @user = User.new
     @title = "Sign Up"
+  end
+  
+  def create
+    @user = User.new params[:user]
+    if @user.save
+      flash[:success] = "Welcome to the Sample App #{@user.name}!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
   
   def show
